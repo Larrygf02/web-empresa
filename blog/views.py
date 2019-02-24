b8185a6bae0c67ad7e65ab886559c228ce090180
larrygf02 <raulgf_02@hotmail.com>
2019-02-24 11:47:30 -0500

    Creando vistas para el blog +1

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from .models import Post
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Category
 # Create your views here.
 
 def blog(request):
@@ -8,3 +8,10 @@ def blog(request):
         'posts':posts,
     }
     return render(request, "blog/blog.html", context)
+
+def category(request, category_id):
+    category = get_object_or_404(Category, id=category_id)
+    context = {
+        'category':category,
+    }
+    return render(request, "blog/category.html", context)
