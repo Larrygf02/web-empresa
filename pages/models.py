a0cb11f52a5cdad8845f3564e5d32df6eafa00c6
larrygf02 <raulgf_02@hotmail.com>
2019-02-25 19:02:34 -0500

    Personalizando el orden de paginas

--- pages/models.py
+++ pages/models.py
@@ -4,13 +4,14 @@ from django.db import models
 class Page(models.Model):
 	title = models.CharField(max_length=200, verbose_name="Titulo")
 	content = models.TextField(verbose_name="Contenido")
+	order = models.SmallIntegerField(verbose_name="Orden", default=0)
 	created = models.DateTimeField(auto_now_add=True, verbose_name="Fecha de creacion")
 	updated = models.DateTimeField(auto_now=True, verbose_name="Fecha de actualizacion")
 
 	class Meta:
 		verbose_name = "pagina"
 		verbose_name_plural = "paginas"
-		ordering = ['title']
+		ordering = ['order','title']
 
 	def __str__(self):
 		return self.title
